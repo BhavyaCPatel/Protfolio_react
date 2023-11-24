7c4c54f8c4b056252b32a31f5799be4bd8736bd4
BhavyaCPatel <bhavyaaes@gmail.com>
2023-11-24 17:02:06 +0530

    Added new Skills and new Projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -26,11 +26,41 @@ return (
             </ul>
             </div>
         </nav>
-    <div className='mx-10 my-10'>
+    <div className='text-center m-5 md:m-10 lg:m-10'>
         <div className=' text-center mt-10' data-aos='zoom-in' data-aos-duration="4000">
             <span className="text-5xl font-semibold pt-20 pb-3 text-dgreen-blue bar">Projects</span>
         </div><br /><br />
-        <div className="container border-2 bg-green-blue rounded-lg my-6" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+            <div className="text-navy-blue" >
+                <h5 className="text-4xl text-center mt-5 ">Shopping Cart</h5>
+                <p className="text-xl p-3">Welcome to ReduxBasket, your go-to shopping cart solution! Manage your items effortlessly with features like adding, removing, updating quantities, deleting items, and clearing the entire cart.
+                </p>
+                <div className="text-center m-5">
+                <a href='https://github.com/BhavyaCPatel/ShoppingCart_Redux' target='__blank' className="bg-gold text-navy-blue px-6 py-3 rounded-full font-semibold hover:bg-transparent hover:border-2 border-navy-blue  hover:text-navy-blue transition duration-300 ease-in-out mx-3">
+                    Source Code
+                </a>
+                <a href='https://reduxbasket.netlify.app/' target='__blank' className="bg-gold text-navy-blue px-6 py-3 rounded-full font-semibold hover:bg-transparent hover:border-2 border-navy-blue hover:text-navy-blue transition duration-300 ease-in-out mx-3">
+                    Live Demo
+                </a>
+                </div>
+            </div>
+        </div>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+            <div className="text-navy-blue" >
+                <h5 className="text-4xl text-center mt-5 ">Task Master</h5>
+                <p className="text-xl p-3">A simple task managing app built with redux-toolkit capable of CRUD operations.
+                </p>
+                <div className="text-center m-5">
+                <a href='https://github.com/BhavyaCPatel/ToDo_Basic' target='__blank' className="bg-gold text-navy-blue px-6 py-3 rounded-full font-semibold hover:bg-transparent hover:border-2 border-navy-blue  hover:text-navy-blue transition duration-300 ease-in-out mx-3">
+                    Source Code
+                </a>
+                <a href='https://taskmasterss.netlify.app/' target='__blank' className="bg-gold text-navy-blue px-6 py-3 rounded-full font-semibold hover:bg-transparent hover:border-2 border-navy-blue hover:text-navy-blue transition duration-300 ease-in-out mx-3">
+                    Live Demo
+                </a>
+                </div>
+            </div>
+        </div>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue" >
                 <h5 className="text-4xl text-center mt-5 ">Avirat Escapes</h5>
                 <p className="text-xl p-3">Avirat Escapes is a ReactJS-based web application for travel and adventure enthusiasts. It simplifies travel planning, offering features like tour packages,trekking adventures, and campsite getaways.
@@ -45,7 +75,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-dgreen-blue rounded-lg my-6" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1400'>
+        <div className="container border-2 bg-dgreen-blue rounded-lg my-6 mx-1" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1400'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">The Informative Corner</h5>
                 <p className="text-xl p-3">Welcome to The Informative Corner! This is my personal blog where I share insights, thoughts, and expertise on a wide range of trending topics. Explore informative articles covering AI, programming, travel, fashion, food, aquascaping, gardening, business, and more.
@@ -60,7 +90,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-green-blue rounded-lg my-6" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Vintage Boys</h5>
                 <p className="text-xl p-3">A sports fashion company it has a main-page with company's overview and a store page with latest fashion, it is built with html, css and bootstrap.
@@ -75,7 +105,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-dgreen-blue rounded-lg my-6" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-dgreen-blue rounded-lg my-6 mx-1" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Apple Clone</h5>
                 <p className="text-xl p-3">A clone of apple's official website built with html, css and bootstrap.
@@ -90,7 +120,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-green-blue rounded-lg my-6" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">ClearSky Forecast</h5>
                 <p className="text-xl p-3">ClearSky Forecast is a weather application that allows users to fetch and view weather information for any city.
@@ -105,7 +135,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-dgreen-blue rounded-lg my-6" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-dgreen-blue rounded-lg my-6 mx-1" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Yelp Camp</h5>
                 <p className="text-xl p-3">Yelp Camp is a camping website that helps outdoor enthusiasts discover the best campsites close to nature and embark on adventurous experiences. Explore our curated collection of campsites and plan your next camping adventure with ease.
@@ -120,7 +150,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-green-blue rounded-lg my-6" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='1000'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Clock</h5>
                 <p className="text-xl p-3">A simple digital clock built using HTML, CSS, and JavaScript. This clock features a dark and light mode toggle for better visibility in different lighting conditions.
@@ -135,7 +165,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-dgreen-blue rounded-lg my-6" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='800'>
+        <div className="container border-2 bg-dgreen-blue rounded-lg my-6 mx-1" data-aos='fade-right' data-aos-duration="4000" data-aos-delay='800'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Banana-Speak</h5>
                 <p className="text-xl p-3">Made a translation web app that will translate your sentences into Banana language.
@@ -150,7 +180,7 @@ return (
                 </div>
             </div>
         </div>
-        <div className="container border-2 bg-green-blue rounded-lg my-6" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='800'>
+        <div className="container border-2 bg-green-blue rounded-lg my-6 mx-1" data-aos='fade-left' data-aos-duration="4000" data-aos-delay='800'>
             <div className="text-navy-blue">
                 <h5 className="text-4xl text-center mt-5 ">Pirate-Speak</h5>
                 <p className="text-xl p-3">Made a translation web app that will translate your sentences into Pirate's language.
--- src/Components/RecentWorks.jsx
+++ src/Components/RecentWorks.jsx
@@ -4,6 +4,8 @@ import avirat from './Assets/avirat.png'
 import blogs from './Assets/blogs.png'
 import applec from './Assets/applec.png'
 import clearsky from './Assets/clearsky.png'
+import shoppingCart from './Assets/shoppingCart.png'
+import todo from './Assets/todo.png'
 import { NavLink } from 'react-router-dom'
 const RecentWorks = () => {
   return (
@@ -12,6 +14,22 @@ const RecentWorks = () => {
           <span className="text-5xl font-semibold pt-20 pb-3 text-dgreen-blue bar">Recent Works</span>
         </div><br /><br />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center m-5" >
+            <div data-aos='zoom-in' data-aos-duration="4000" data-aos-delay='1000'>
+              <Cards
+              image={shoppingCart}  
+              title='Shopping Cart' 
+              desc='Welcome to ReduxBasket, your go-to shopping cart solution! Manage your items effortlessly with features like adding, removing, updating quantities, deleting items, and clearing the entire cart.'
+              source='https://github.com/BhavyaCPatel/ShoppingCart_Redux'
+              demo='https://reduxbasket.netlify.app/'/>
+            </div>
+            <div data-aos='zoom-in' data-aos-duration="4000" data-aos-delay='1000'>
+              <Cards
+              image={todo}
+              title='Task Master' 
+              desc='Welcome to TaskMaster, your task management solution! View and manage your tasks seamlessly with features like add, delete, and update.'
+              source='https://github.com/BhavyaCPatel/ToDo_Basic'
+              demo='https://taskmasterss.netlify.app/'/>
+            </div>
             <div data-aos='zoom-in' data-aos-duration="4000" data-aos-delay='1000'>
               <Cards
               image={avirat}  
